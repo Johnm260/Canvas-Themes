bb3a2d5f0f28b20799768cea2c8d4b3924d02616
Johnm260 <122632896+Johnm260@users.noreply.github.com>
2023-11-15 13:29:01 -0600

    Update updating-bookmarklet.js

--- updating-bookmarklet.js
+++ updating-bookmarklet.js
@@ -1,6 +1,6 @@
 (function() {
     'use strict';
     var script = document.createElement('script');
-    script.src = "https://cdn.jsdelivr.net/gh/Johnm260/Canvas-Themes@latest/Code.js";
+    script.src = "https://cdn.jsdelivr.net/gh/Johnm260/Canvas-Themes/Code.js";
     document.body.appendChild(script);
 })();
